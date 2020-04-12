e33b1522f28a64567ea37a276cc3ce5d7ec21637
Jeff Kent <jeff@jkent.net>
2020-04-12 18:24:41 -0500

    demo code, using esp32-caliper submodule

--- main/main.c
+++ main/main.c
@@ -1,43 +1,24 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <stdio.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+
+#include "caliper.h"
+
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    caliper_init();
+    struct caliper* caliper = caliper_add(18, 23);
+    assert(caliper);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Free heap: %d\n", esp_get_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    while (true) {
+        caliper_mode_t mode;
+        bool power;
+        double value = caliper_value(caliper, &mode, &power);
+        if (mode == CALIPER_MODE_INCHES) {
+            printf("%0.4f in (%s)\n", value, power ? "on" : "off");
+        } else {
+            printf("%0.2f mm (%s)\n", value, power ? "on" : "off");
+        }
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
