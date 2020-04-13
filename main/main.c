eb4598a06738ca9a1ff38690ee8a730561821ec8
Jeff Kent <jeff@jkent.net>
2020-04-13 02:42:18 -0500

    update esp32-caliper

--- main/main.c
+++ main/main.c
@@ -5,20 +5,31 @@
 #include "caliper.h"
 
 
+static void caliper_cb(caliper_handle handle, double value, caliper_units_t units, bool power)
+{
+    if (units == CALIPER_UNITS_MM) {
+        printf("%0.2f mm (%s)\n", value, power ? "on" : "off");
+    } else {
+        printf("%0.4f in (%s)\n", value, power ? "on" : "off");
+    }
+}
+
 void app_main(void)
 {
     caliper_init();
-    struct caliper* caliper = caliper_add(18, 23);
-    assert(caliper);
 
-    while (true) {
-        caliper_mode_t mode;
-        bool power;
-        double value = caliper_value(caliper, &mode, &power);
-        if (mode == CALIPER_MODE_INCHES) {
-            printf("%0.4f in (%s)\n", value, power ? "on" : "off");
-        } else {
-            printf("%0.2f mm (%s)\n", value, power ? "on" : "off");
-        }
+    caliper_handle handle = caliper_add(18, 23, caliper_cb);
+    assert(handle);
+
+    double value;
+    caliper_units_t units;
+    bool power;
+
+    caliper_poll(handle, &value, &units, &power);
+
+    if (units == CALIPER_UNITS_MM) {
+        printf("%0.2f mm (%s)\n", value, power ? "on" : "off");
+    } else {
+        printf("%0.4f in (%s)\n", value, power ? "on" : "off");
     }
 }
